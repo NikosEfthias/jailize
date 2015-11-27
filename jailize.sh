6cd6d2e5bf67a75a15967357404d0860547ee747
Nikos Efthias <nikos.efthias@gmail.com>
2015-11-27 14:53:25 +0300

    naming support added

--- jailize.sh
+++ jailize.sh
@@ -8,8 +8,8 @@ bold="\033[1m"
 reset="\033[0m"
 ################
 ###defaults#####
-dirNum=1;#default number of jails to build
 baseDir=$(pwd)
+nameDir="jail"
 cmd=$(whoami)
 bins="su ls rm mv bash"
 if [[ $cmd != "root" ]]; then
@@ -20,12 +20,9 @@ fi
 ######make dirs######
 dirMake(){
   echo "Creating Jails"
-$(eval "mkdir $baseDir/jail{1..$dirNum}")
-  for i in $(eval "echo {1..$dirNum}") ;do
-    printf "\033[32mjail$i is created creating config folder inside\033[0m\n"
-    $(eval "mkdir $baseDir/jail$i/{bin,lib,dev,etc,lib64}")
-    # $(chmod -R 0111 $baseDir/jail$i)
-  done
+  $(mkdir $baseDir/$nameDir)
+  printf "\033[32m$nameDir is created creating config folder inside\033[0m\n"
+  $(eval "mkdir $baseDir/$nameDir/{bin,lib,dev,etc,lib64}")
 }
 ###################
 ###input required##
@@ -43,49 +40,40 @@ do
   echo $i
   binAddr=$(which $i)
   if [[ $? = 0 ]]; then
-    printf "\033[32m cp $binAddr --> jail/bin\033[0m\n"
-    $(eval "mkdir -p jail{1..$dirNum}$(dirname $binAddr)")
-    $(eval "cp $binAddr $baseDir/jail{1..$dirNum}$binAddr")
+    printf "\033[32m cp $binAddr --> $nameDir/bin\033[0m\n"
+    $(mkdir -p $baseDir/$nameDir$(dirname $binAddr))
+    $(cp  $binAddr $baseDir/$nameDir$binAddr)
     ldd=$(ldd $binAddr |cut -d ">" -f2|cut -d "(" -f1)
     for l in $ldd ;do
-      if [[ $l ]]; then
-          $(eval "mkdir -p $baseDir/jail{1..$dirNum}$(dirname $l)")
-        printf "\033[32m cp $l --> jail$l\033[0m\n"
-      $(eval "cp $l $baseDir/jail{1..$dirNum}$l")
+      if [[ -n $l ]]; then
+          $(mkdir -p $baseDir/$nameDir$(dirname $l))
+          printf "\033[32m cp $l --> $baseDir/$nameDir$l\033[0m\n"
+          $(cp $l $baseDir/$nameDir/.$l)
       fi
-    done
+  done
   else
     printf "\033[31m$i cannot be found You may need to copy it manually\033[0m
 Hit enter to continue or ctrl+c to quit script\n"
 read
   fi
 done
-# $(eval "ln -s $baseDir/jail1/lib/* $baseDir/jail{1..$dirNum}/lib64/")
 }
 ################
 ###Parse Command Line Args########
 while [[ -n $1 ]];do
-# while getopts "n:hd:b:a:" flg
-#n number folders;
-# h help text; d basedir;
-#b whitespace seperated binary list to put into jail
-# append binary to defaults
-
+# run wcript with -h flag to see help text
    if [[ $1 =~ ^[^-].* ]] ; then
      printf "\033[31mI dont understand what you meant with \033[7m$1\033[0m\n"
      exit 1
    fi
 case $1 in
-  -n)
-  reqinp $1 $2
-  dirNum=$2
-  shift 2 ;;
   -h)
-  params=("-n" "-d" "-b" "-a" "--add-rec" "-h")
+  params=("-n" "--name" "-d" "-b" "-a" "--add-rec" "-h")
   printf "\nCreate Chroot Jails
 \nUsage jailize.sh [options]
   Options
-    %s            Number of parallel chroot jail folders default is 1
+    %s            name of the folder default=jail
+    %s            same as n oprion but more verbose
     %s            Base directory if no given current working directory is default
     %s            Whitespace seperated binary list string to put each with dependent libraries into jails
           default value is 'su ls rm mv bash'
@@ -94,9 +82,12 @@ case $1 in
     %s            Show this help text\n\n" ${params[@]}
     exit 1
     ;;
-
-    -d)
+    -n|--name|-name)
       reqinp $1 $2
+      nameDir=$2
+      shift 2
+      ;;
+    -d)
      if [[ -d $2 ]]; then
       baseDir=$2
       shift 2
@@ -125,7 +116,7 @@ printf "\033[32mStarting ...\033[0m\n"
 printf "\033[36mProgram variables$reset
 Base Dir                          : $baseDir
 Binaries to put into jail         : $bins
-Numbers of parallel Jails         : $dirNum
+Name of the Jail                  : $nameDir
 \033[35mHit enter to continue or ctrl -c to purge config and start over\033[1A\033[0m\n"
 read
 dirMake
